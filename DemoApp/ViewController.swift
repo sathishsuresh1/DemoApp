89246b12ed99b3f3de8507dbad01496b33c34ea6
sathishsuresh1 <sathishsuresh1@outlook.com>
2024-09-19 13:40:19 +0400

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Sathish Suresh on 19/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DemoAppTests/DemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppTests.swift
+//  DemoAppTests
+//
+//  Created by Sathish Suresh on 19/09/24.
+//
+
+import Testing
+@testable import DemoApp
+
+struct DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
